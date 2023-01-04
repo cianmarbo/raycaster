f66bc655d1fe2b1abff57a727d383b3199c8d055
cianmarbo <cianmarbo@gmail.com>
2023-01-04 16:40:17 +0000

    Adding raycast.c

---
+++ raycast.c
@@ -0,0 +1,26 @@
+#define TILE_SIZE 48
+#define MAP_NUM_ROWS 11
+#define MAP_NUM_COLS 15
+
+#define WINDOW_WIDTH  (TILE_SIZE * MAP_NUM_COLS)
+#define WINDOW_HEIGHT (TILE_SIZE * MAP_NUM_ROWS)
+
+#define PI 3.141592
+
+#define FOV_ANGLE (60 * (PI / 180))
+
+#define WALL_STRIP_WIDTH 2
+#define NUM_RAYS (WINDOW_WIDTH / WALL_STRIP_WIDTH)
+
+#define MINIMAP_SCALE_FACTOR 0.2
+
+int main(int argc, char** argv) {
+
+	
+
+
+
+
+	return 0;
+}
+
